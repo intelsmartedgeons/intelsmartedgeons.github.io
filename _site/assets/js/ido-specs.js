4f9e15aae650744f18daa431d90a36ea1c2144f4
Sheetal <sheetal.saraswat@onsumaye.com>
2021-10-04 16:17:15 +0530

    scss update

--- _site/assets/js/ido-specs.js
+++ _site/assets/js/ido-specs.js
@@ -130,12 +130,12 @@ jQuery(document).ready(function(){
     jQuery('.xbutton').on('click',function(e){e.preventDefault();
 		jQuery('#alertMsg').remove();
 	});
-	 jQuery("#contact-button").click(function() {
+    jQuery("#contact-button").click(function() {
         if(jQuery(this).css("left") == "-319px"){
-            jQuery(this).parent().animate({"left": "0px"});
+            jQuery(this).animate({"left": "0px"});
         }
         else{
-            jQuery(this).parent().animate({"left": "-319px"});
+            jQuery(this).animate({"left": "-319px"});
          }
         if(jQuery(this).parent().css("left") == "-320px"){
             jQuery(this).parent().animate({"left": "0px"});
